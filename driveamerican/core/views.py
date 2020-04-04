3cd0d97326ed8aaebed10e7ba9f9f5fdf97d04f4
viktor.orkush <viktor.orkush@gmail.com>
2020-04-04 22:26:47 +0300

    change file sitemap.xml

--- driveamerican/core/views.py
+++ driveamerican/core/views.py
@@ -8,6 +8,7 @@ def robots_txt(request):
     lines = [
         "User-agent: Googlebot\n",
         "User-agent: *\n",
+        "Disallow: /admin/\n",
         "Sitemap: http://driveamerican.top/sitemap.xml\n",
     ]
     return HttpResponse("\n".join(lines), content_type="text/plain")
