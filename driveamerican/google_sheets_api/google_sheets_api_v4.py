12dc9664259eeea55e4193bb3485097de7a555ad
viktor.orkush <viktor.orkush@gmail.com>
2020-04-07 21:30:31 +0300

    change description in template calculation.html

--- driveamerican/google_sheets_api/google_sheets_api_v4.py
+++ driveamerican/google_sheets_api/google_sheets_api_v4.py
@@ -14,7 +14,8 @@ django.setup()
 from driveamericanapp.models import Auction, TransportationPrice
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-SAMPLE_SPREADSHEET_ID = '1IG13mYrF1FGEWeFQ0ZaJfFVvYUOFVxiGZylVD8wxEic'
+# SAMPLE_SPREADSHEET_ID = '1IG13mYrF1FGEWeFQ0ZaJfFVvYUOFVxiGZylVD8wxEic'
+SAMPLE_SPREADSHEET_ID = '1EX5zEpBjmcHGzMVYcIw7_j6KXr9NrJyG'
 
 
 def connect_sheets():
@@ -28,29 +29,28 @@ def connect_sheets():
 
 def import_data_from_sheets_to_db():
     result = connect_sheets().values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                           range='A4:J',
+                                           range='A1:E',
                                            majorDimension='ROWS').execute()
     values = result.get('values', [])
 
     for row in values:
         print('%s' % (row))
-        # print('row9' + re.sub('\D', '', row[9]))
         if row[1] == 'IAAI' or row[1] == 'Copart':
             auction = Auction.objects.get(auction=row[1])
             # print(re.sub(r'[^A-Za-z\s]', '', row[0]))
             # print(re.split(r'[,(\-!?:]+', row[0]))
             print(re.sub(r'[^A-Za-z\s]', '', re.split(r'[,(!?:]+', row[0])[0]))
-            print('savannah =  ' + str(str_to_int(re.sub('\D', '', row[5]))))
+            print('savannah =  ' + str(str_to_int(re.sub('\D', '', row[4]))))
 
-            TransportationPrice.objects.create(auction=auction,
-                                               auction_location=re.sub(r'[^A-Za-z\s]', '',
-                                                                       re.split(r'[,(!?:]+', row[0])[0]),
-                                               city=row[2], state=row[3], zip=row[4],
-                                               port_savannah=str_to_int(re.sub('\D', '', row[5])),
-                                               port_newark=str_to_int(re.sub('\D', '', row[6])),
-                                               port_houston=str_to_int(re.sub('\D', '', row[7])),
-                                               port_los_angeles=str_to_int(re.sub('\D', '', row[8])),
-                                               port_indianapolis=str_to_int(re.sub('\D', '', row[9])))
+            # TransportationPrice.objects.create(auction=auction,
+            #                                    auction_location=re.sub(r'[^A-Za-z\s]', '',
+            #                                                            re.split(r'[,(!?:]+', row[0])[0]),
+            #                                    city=row[2], state=row[3], zip=row[4],
+            #                                    port_savannah=str_to_int(re.sub('\D', '', row[5])),
+            #                                    port_newark=str_to_int(re.sub('\D', '', row[6])),
+            #                                    port_houston=str_to_int(re.sub('\D', '', row[7])),
+            #                                    port_los_angeles=str_to_int(re.sub('\D', '', row[8])),
+            #                                    port_indianapolis=str_to_int(re.sub('\D', '', row[9])))
 
 
 def str_to_int(value):
